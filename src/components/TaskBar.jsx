e876cb9527814526d8c484d0f52fdebcc5cfc703
Heytemisan <temyicy@gmail.com>
2022-06-13 05:02:18 +0100

    chore: style fixed"

--- src/components/TaskBar.jsx
+++ src/components/TaskBar.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const TaskBar = () => {
     return (
-        <div>TaskBar</div>
+        <div className="taskContainer">
+            <div>
+                <div className='tasks'>
+                    <p>Tasks</p>
+                    <p>0</p>
+                </div>
+                <div className="plus">
+                <p>+</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
